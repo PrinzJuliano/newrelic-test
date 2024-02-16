7f284edc6c637cf02d60ea7d75ff5dde3f3bb405
PrinzJuliano <PrinzJuliano@users.noreply.github.com>
2024-02-16 10:33:40 +0100

    Added android newrelic instrumentation

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,20 @@
 package io.ionic.starter;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 
-public class MainActivity extends BridgeActivity {}
+import androidx.annotation.Nullable;
+
+import com.getcapacitor.BridgeActivity;
+import com.newrelic.agent.android.NewRelic;
+
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    NewRelic.withApplicationToken(
+      "eu01xxfc517fc0d4b05fc00c907513afd54a592863-NRMA"
+    ).start(this.getApplicationContext());
+  }
+}
